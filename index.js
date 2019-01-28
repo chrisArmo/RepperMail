d9027285fb6a0691b0bce79c7c7c47728f32e4a1
Chris Armendarez <chris.michael.armo@gmail.com>
2019-01-27 17:26:17 -0800

    Add initial express setup: index.js

--- index.js
+++ index.js
@@ -2,4 +2,23 @@
  * Repper Mail Server
  */
 
+// Dependencies
+ 
+const express = require("express");
 
+// Application setup
+
+const app = express(),
+    PORT = process.env.PORT || 3000;
+
+// Routes
+
+app.get("/", (req, res) => {
+    res.status(200).json({ message: "Hello, world!" });
+});
+
+// Listen
+
+app.listen(PORT, () => {
+    console.log(`Repper Mail server running on port ${PORT}`);
+});
